54d7a75d8e8b19c8fcec03fbbbcd90795a807ea0
petrik <petriksinambela12@gmail.com>
2025-09-09 02:00:39 +0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -33,7 +33,7 @@ const Footer: React.FC = () => {
   ];
 
   const handleWhatsAppContact = () => {
-    const phoneNumber = '6282360171945';
+    const phoneNumber = '6285366829582';
     const message = `Halo! Saya tertarik untuk bergabung dengan Komunitas Kobar.`;
     const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     window.open(whatsappUrl, '_blank');
